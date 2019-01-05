65fc13ec4eca4dc6ca7e0d7c532742c18eaf977b
suthanon97 <suthanon97@zoho.com>
2019-01-06 03:47:11 +0800

    Changes to database

---
+++ projectDotNetv2/Manage Booking.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Manage Booking.aspx.cs" Inherits="projectDotNetv2.WebForm3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 62px;
+            height: 28px;
+        }
+        .auto-style2 {
+            height: 28px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server">
+                <Columns>
+            <asp:BoundField DataField="LecturerName" HeaderText="Lecturer" />
+            <asp:BoundField DataField="DateTime" HeaderText="Date and Time" />
+            <asp:BoundField DataField="Description" HeaderText="Description" />
+            <asp:BoundField DataField="Status" HeaderText="Status" />
+                    <asp:TemplateField>
+                       <ItemTemplate>
+                           <asp:LinkButton ID="linkaccept" runat="server" CommandArgument="<% Accept ('MeetingID' %>"> Accept</asp:LinkButton>
+                           <asp:LinkButton ID="LinkDecline" runat="server" CommandArgument="<% Decline('MeetingID' %>"> Decline</asp:LinkButton>
+                       </ItemTemplate>
+
+                    </asp:TemplateField>
+
+                </Columns>
+
+
+
+            </asp:GridView>
+            
+
+
+            <asp:Panel ID="Panel1" runat="server" > </asp:Panel>
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ projectDotNetv2/Manage Booking.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text;
+
+namespace projectDotNetv2
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string mainconn = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+                SqlConnection sqlconn = new SqlConnection(mainconn);
+                string sqlquery = "select * from [dbo].[BookMeeting]";
+                SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+                sqlconn.Open();
+                SqlDataAdapter sda = new SqlDataAdapter(sqlcomm);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                StringBuilder sb = new StringBuilder();
+                sb.Append("<center>");
+                sb.Append("<h1>How to bind data</h1>");
+                sb.Append("<h2>using stringbuilder  class</h2>");
+                sb.Append("<hr/>");
+                sb.Append("<table border=1>");
+                sb.Append("<tr>");
+                foreach (DataColumn dc in dt.Columns)
+                {
+                    sb.Append("<th>");
+                    sb.Append(dc.ColumnName.ToUpper());
+                    sb.Append("</th>");
+
+
+                }
+                sb.Append("</tr>");
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    sb.Append("<tr>");
+                    foreach (DataColumn dc in dt.Columns)
+                    {
+                        sb.Append("<th>");
+                        sb.Append(dr[dc.ColumnName].ToString());
+                        sb.Append("</th>");
+                    }
+                    sb.Append("</tr>");
+
+                }
+                sb.Append("</table>");
+                sb.Append("</center>");
+                Panel1.Controls.Add(new Label { Text = sb.ToString() });
+                sqlconn.Close();
+
+            }
+        }
+    }
+}
